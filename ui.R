d304cd7a15bfbc618e332bf09f60521cafe4178d
Andre Vieira <ahpvieira@gmail.com>
2020-03-25 15:51:56 -0300

    clean ui

--- ui.R
+++ ui.R
@@ -15,9 +15,6 @@ fluidPage(title = "",
           
           
           # CSS ----
-          # tags$head(
-          #       tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
-          # ),
           includeCSS("www/styles.css"),
           
           # JS ----
@@ -26,11 +23,7 @@ fluidPage(title = "",
           # 1.0 HEADER ----
           div(
                 class = "container-fluid",
-                # div(
-                # class = "panel panel-default" ,
-                # div(
-                # class = "panel-body",
-                # id = "header",
+                
                 shinydashboard::box(
                       solidHeader = TRUE,
                       width = 12,
@@ -38,18 +31,14 @@ fluidPage(title = "",
                             class = "page-header",
                             "Coronavírus COVID-19 – Casos no Brasil segundo dados oficiais das secretarias estaduais de saúde")
                 )
-                # )
-                # )
-          ),
+                ),
           
-          # 2.0 FAVORITES ----
           div(
                 # class = "container hidden-sm hidden-xs",
                 id = "favorite_container",
                 
                 div(
                       class = "container-fluid",
-                      # style = "margin-right:-24px; margin-left:-24px;",
                       column(
                             width = 2,
                             style = 'padding:4px;',
@@ -62,12 +51,7 @@ fluidPage(title = "",
                                            "Total confirmado"),
                                         h1(style = "margin: 10px; font-size: 44px; font-weight: bold; color: rgb(230, 0, 0); text-align: center;", 
                                            textOutput("total_casos"))
-                                        # p(
-                                        #       class = str_glue("text-{sub_text_color}"),
-                                        #       icon(sub_icon),
-                                        #       tags$small(sub_value)
-                                        # )
-                                  )
+                                        )
                             ),
                             div(
                                   class = "panel panel-default",
@@ -116,10 +100,9 @@ fluidPage(title = "",
                                         shinydashboard::box(
                                               style = 'overflow-y: scroll; width: 100%; height: 40px; margin-top: -10px;',
                                               width = 12,
-                                              # height = "100%",
-                                              # background = "light-blue",
                                               HTML("<h6 style ='line-height: 18px;'><b style='color:white'>Fontes dos dados:</b> Secretarias estaduais de saúde e Ministério da Saúde. <br>
-                                              <b style='color:white'> Dados disponíveis para download:</b> GitHub: <a href='https://github.com/wcota/covid19br'>Aqui</a>. <br>
+                                              <b style='color:white'>Dados disponíveis para download:</b> GitHub: <a href='https://github.com/wcota/covid19br'>Aqui</a>. <br>
+                                              <b style='color:white'>Código:</b> GitHub: <a href='https://github.com/andrepvieira/coronavirus_brasil'>Aqui</a>. <br>
                                               <b style='color:white'>Desenvolvimento:</b> <a href='https://www.linkedin.com/in/andr%C3%A9-vieira-16095557/'>André Vieira</a>.</h6>")
                                         )
                                   )
@@ -141,25 +124,10 @@ fluidPage(title = "",
                                         DT::dataTableOutput('table_mortes', height = "100%")
                                   )
                             ),
-                            
-                            # div(
-                            #       class = "panel panel-default",
-                            #       style = "margin-top: -12px;",
-                            #       style = "height: 187px;",
-                            #       div(
-                            #             class = str_glue("panel-body bg-default text-default"),
-                            #             h5(style = 'margin: 2px; color: #bdbdbd; text-align: center; font-weight: bold;', 
-                            #                "Óbitos por unidade da Federação"),
-                            #             DT::dataTableOutput('table_mortes')  
-                            #             
-                            #       )
-                            # )
-                            
-                      ),
+                            ),
                       column(
                             width = 2,
                             style = 'padding:4px;',
-                            # style = 'padding-left:4px;',
                             div(
                                   class = "panel panel-default",
                                   div(
@@ -170,22 +138,10 @@ fluidPage(title = "",
                                         h1(style = "margin: 10px; font-size: 44px; font-weight: bold; color: rgb(112, 168, 0); text-align: center;", 
                                            "--"),
                                         p("Dados não disponíveis.")
-                                        
                                         # DT::dataTableOutput('table_recuperado')
                                   )
                             ),
-                            # div(
-                            #       class = "panel panel-default",
-                            #       style = "    margin-top: -12px;",
-                            #       style = "    height: 187px;",
-                            #       
-                            #       div(
-                            #             class = str_glue("panel-body bg-default text-default"),
-                            #             h5(style = 'margin: 2px; color: #bdbdbd; text-align: center; font-weight: bold;', 
-                            #                "Recuperados por unidade da Federação"),
-                            #       )
-                            # )
-                      ),
+                            ),
                       column(
                             width = 4,
                             style = "margin-top: -8px;",
@@ -196,7 +152,6 @@ fluidPage(title = "",
                                   style = "height: 275px;",
                                   
                                   div(
-                                        # class = str_glue("panel-body bg-default text-default"),
                                         tabsetPanel(
                                               tabPanel("Acumulado",
                                                        highcharter::highchartOutput("chart_acumulado", 
@@ -218,30 +173,7 @@ fluidPage(title = "",
                                   )
                             )
                       )
-                      # )
-                      # fluidRow(
-                      #       column(
-                      #             width = 4,
-                      #             style='padding:4px;',
-                      #       div(
-                      #             class = "panel panel-default",
-                      #             style = "    margin-top: -12px;",
-                      #             style = "    height: 225px; width: 200px;",
-                      #             
-                      #             div(
-                      #                   class = str_glue("panel-body bg-default text-default"),
-                      #                   p("Gráfico de linha")
-                      #             )
-                      #       )
-                      # )
-                      # )
-                      
-                      
-                )
+                      )
           )
           
-          # 3.0 APPLICATION UI -----
-          
-          
-          
-)
+       )
